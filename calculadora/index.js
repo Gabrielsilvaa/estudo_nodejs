3fdd1053b0ccbde40478aa3e732b4b3bcea4a000
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-06-24 14:57:44 -0300

    Add files via upload

---
+++ calculadora/index.js
@@ -0,0 +1,19 @@
+/*aqui vamos fazer varias maneiras de
+calcular numeros, iremos aplicar as quatros 
+operaçoes aritimeticas*/
+
+//primeriro metodo
+//voce pode definir tanto um constante quanto uma variavel
+const num1 = 3;
+var num2 = 3
+soma = num1 + num2;
+subtracao = num1 - num2;
+divisao = num1 / num2;
+multiplicao = num1 * num2;
+
+console.log("O resultado das operacoes é, soma " + soma + " subtração " + subtracao + " divião " + divisao + " multplicação " + multiplicao);
+
+//segundo metodo 
+console.log('');
+console.log('------- segundo metodo -------');
+
